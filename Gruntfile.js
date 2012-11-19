58565cca81b1538a5535a1d2fe3ca0ccab2228e5
Tyler Kellen <tyler@sleekcode.net>
2012-11-19 16:28:37 -0500

    fix test suite in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -62,9 +62,9 @@ module.exports = function(grunt) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['nodeunit']);
+  grunt.registerTask('test', ['clean', 'requirejs', 'nodeunit']);
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['requirejs', 'test', 'build-contrib']);
+  grunt.registerTask('default', ['jshint', 'test', 'build-contrib']);
 
 };
