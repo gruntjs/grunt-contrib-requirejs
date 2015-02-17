62e6ce7383752e0a79bbe47c373011a3f693f281
Nuno Santos <nunofvsantos@gmail.com>
2015-02-17 18:58:15 +0100

    Added support for options.banner.
    This is implemented by overriding the options.out parameter that is
    passed to requirejs.optimize(). This method will prepend the banner and
    write the result to the output file (since requirejs does no writing
    when options.out is overridden).
    
    Related to issue #58.

--- tasks/requirejs.js
+++ tasks/requirejs.js
@@ -10,6 +10,8 @@ module.exports = function(grunt) {
   'use strict';
 
   var requirejs = require('requirejs');
+  var fs = require('fs');
+  var path = require('path');
   var LOG_LEVEL_TRACE = 0, LOG_LEVEL_WARN = 2;
 
   // TODO: extend this to send build log to grunt.log.ok / grunt.log.error
@@ -29,6 +31,7 @@ module.exports = function(grunt) {
 
     var done = this.async();
     var options = this.options({
+      banner: '',
       logLevel: grunt.option('verbose') ? LOG_LEVEL_TRACE : LOG_LEVEL_WARN,
       done: function(done, response){
         done();
@@ -42,6 +45,15 @@ module.exports = function(grunt) {
         grunt.fail.warn('There was an error while processing your done function: "' + e + '"');
       }
     };
+
+    if (options.banner) {
+      var banner = options.banner.replace(/\r\n/g, '\n');
+      delete options.banner;
+      var outputFile = options.out;
+      options.out = function(text) {
+        fs.writeFileSync(path.resolve(outputFile), banner + text);
+      };
+    }
 
     requirejs.optimize(options, tryCatch.bind(null, options.done, done));
   });
